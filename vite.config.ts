228fdc4c2c7966792d2513e3f87ed4efa1f64020
Daniel Lahlafi <daniellahlafi@gmail.com>
2025-05-07 14:44:30 -0700

    Fix routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -12,8 +13,6 @@ export default defineConfig({
   server: {
     fs: {
       strict: true
-    
-    },
-    
+    }
   }
 })
